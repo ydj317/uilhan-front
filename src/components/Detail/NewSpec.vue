8b3d5909fe388873f824fb830238f43f202afd6b
baichangjie <changjie201@hotmail.com>
2023-06-15 22:32:22 +0800

    액션 추가

--- src/components/Detail/NewSpec.vue
+++ src/components/Detail/NewSpec.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="eModelTitle_2_1" class="mt20 p20 bg-white" v-if="product.item_option">
     <h1><strong>New 옵션설정</strong></h1>
+    <!--옵션그룹해더, 옵션 셋팅, 옵션그룹 추가-->
     <div class="header-section">
       <!--세팅버튼-->
       <div class="setting header-button">
@@ -11,23 +12,27 @@
           <a-button class="floatRight" type="primary" danger>세팅</a-button>
         </a-popconfirm>
       </div>
-      <a-button class="header-button" type="primary">추가</a-button>
+      <a-button @click="addSpecGroup" class="header-button" type="primary">추가</a-button>
     </div>
+    <!--옵션그룹 영역-->
     <div class="body-section">
-      <div class="spec-box">
+      <div class="spec-box" v-for="(option, optionIndex) in product.item_option" :key="optionIndex">
         <table>
+          <!--옵션그룹,옵션일괄설정-->
           <thead>
           <tr>
+            <!--그룹명 영역-->
             <th style="background-color: #ebeff0;">
               <div class="spec-option-group">
-                <span class="spec-font">옵션그룹1</span>
-                <a-input class="spec-option-input-size" v-model:value="value" size="default" placeholder="small size" />
-                <span class="spec-count">10 / 10</span>
-                <a-button class="spec-grop-delete" type="primary" danger>삭제</a-button>
+                <span class="spec-font">옵션그룹{{optionIndex + 1}}</span>
+                <a-input class="spec-option-input-size"  v-model:value="option.name" size="default" placeholder="Basic usage" />
+                <span class="spec-count"><span>{{ option.name.length }}</span> / 25</span>
+                <a-button @click="deleteSpecGroup(optionIndex)" class="spec-grop-delete" type="primary" danger>삭제</a-button>
               </div>
             </th>
           </tr>
           <tr>
+            <!--옵션값 일괄 설정 영역-->
             <th>
               <div class="spec-option-header">
                 <div class="spec-option-left">
@@ -41,20 +46,21 @@
                   <a-button class="spec-right-button" type="primary">25자</a-button>
                   <a-button class="spec-right-button" type="primary">순번</a-button>
                   <a-button class="spec-right-button" type="primary">A-Z</a-button>
-                  <a-button type="primary">뒤로</a-button>
+                  <a-button type="primary">Back</a-button>
                 </div>
               </div>
             </th>
           </tr>
           </thead>
+          <!--옵션명 영역-->
           <tbody>
-          <tr>
+          <tr v-for="(item, index) in option.data" :key="item.key">
             <td>
               <div class="spec-option-name">
                 <a-checkbox class="spec-checkbox" v-model:checked="checked"></a-checkbox>
-                <a-input class="input-size" v-model:value="value" size="default" placeholder="small size" />
-                <span class="spec-count">10 / 10</span>
-                <a-button class="spec-add-option-name-button" type="primary">추가</a-button>
+                <a-input class="input-size" v-model:value="item.name" size="default" placeholder="small size" />
+                <span class="spec-count">{{ item.name.length }} / 25</span>
+                <a-button @click="addSpecOptionName(optionIndex)" v-if="option.data.length === (index + 1)" class="spec-add-option-name-button" type="primary">추가</a-button>
               </div>
             </td>
           </tr>
@@ -65,6 +71,8 @@
   </div>
 </template>
 <script>
+import { lib } from "util/lib";
+import { forEach } from "lodash";
 import { mapState } from "vuex";
 
 export default {
@@ -74,9 +82,114 @@ export default {
   },
   data() {
     return {
+      temp_sku: [],
     };
   },
   methods: {
+    setting() {
+      this._getSku([], 0, this.product.item_option);
+      this._setSku();
+    },
+    addSpecGroup() {
+      this.product.item_option.push({
+        name: "",
+        data: [
+          {key:this._uniqueKey(), name: ''}
+        ]
+      });
+    },
+    deleteSpecGroup(optionIndex) {
+      this.product.item_option.splice(optionIndex, 1);
+    },
+    addSpecOptionName(optionIndex) {
+      this.product.item_option[optionIndex].data.push({key:this._uniqueKey(), name: ''});
+    },
+    _uniqueKey() {
+      return ((1 + Math.random()) * 0x10000) | 0;
+    },
+    _getSku(aTempSku, i, aOptions) {
+      forEach(aOptions[i].data, (val, key) => {
+        if (i < aOptions.length - 1) {
+          aTempSku[i] = aOptions[i].data[key];
+          this._getSku(aTempSku, i + 1, aOptions);
+        } else {
+          this.temp_sku.push([...aTempSku, aOptions[i].data[key]]);
+        }
+      });
+    },
+    _setSku() {
+      let format_sku = [];
+      let aOptionImage = this._getOptionImage();
+
+      forEach(this.temp_sku, (sku) => {
+        let sName = sku[0].name;
+        if (sku.length > 1) {
+          // sName = `${sku[0].name}::${sku[1].name}`;
+          let aTempName = [];
+          forEach(sku, (_sku) => {
+            aTempName.push(_sku.name);
+          });
+          sName = aTempName.join("::");
+        }
+
+        let bPush = true;
+        forEach(this.product.sku, (original_sku) => {
+          // let aName = original_sku.spec.split('::');
+          if (original_sku.spec === sName) {
+            bPush = false;
+            // 기존 sku 는 그대로 유지
+            format_sku.push(original_sku);
+          }
+        });
+
+        // 새로 추가된 sku
+        if (bPush) {
+          // 셋팅시 새로은 품목 기존 옵션이미지 사용
+          let sImageUrl = "";
+          sName.split("::").map((tempSkuName) => {
+            if (!lib.isEmpty(aOptionImage[tempSkuName])) {
+              sImageUrl = aOptionImage[tempSkuName];
+            }
+          });
+
+          // 축소된 이미지 원복 (원복할 이미지가 아니면 기존이미지)
+          sImageUrl = lib.getOraginalImageUrlToString(sImageUrl, ['.jpg', '.jpeg', '.bmp', '.png', '.gif']);
+
+          format_sku.push({
+            img: sImageUrl,
+            code: "",
+            spec: sName,
+            stock: 0,
+            original_price_cn: 0,
+            original_price_ko: 0,
+            shipping_fee_cn: 0,
+            shipping_fee_ko: 0,
+            wholesale_price: 0,
+            disp_price: 0,
+            selling_price: 0,
+            key: ((1 + Math.random()) * 0x10000) | 0,
+          });
+        }
+      });
+
+      this.product.sku = JSON.parse(JSON.stringify(format_sku));
+    },
+    _getOptionImage() {
+      let aOptionImages = {};
+      this.product.item_option.map((item_option) => {
+        let aTemp = item_option.data.filter(
+            (option) => !lib.isEmpty(option.img)
+        );
+
+        if (lib.isArray(aTemp, true)) {
+          aTemp.map((item) => {
+            aOptionImages[item.name] = item.img;
+          });
+        }
+      });
+
+      return aOptionImages;
+    },
   }
 };
 </script>
@@ -97,7 +210,7 @@ export default {
 
 .body-section {
   display: flex;
-  justify-content:center;
+  flex-direction:row;
   width: 100%;
   height: 90%;
   overflow: auto;
