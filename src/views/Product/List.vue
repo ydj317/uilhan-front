6b141255c010728e02adc2183faed61d4b0bdd7d
scson <763761452@qq.com>
2023-06-17 09:27:42 +0800

    update

--- src/views/Product/List.vue
+++ src/views/Product/List.vue
@@ -227,7 +227,6 @@
 
         <template v-slot:footer>
           <a-button type="primary" @click="testsync('single')">선택제휴사연동</a-button>
-          <a-button type="primary" @click="closeResultPop('single')">확인</a-button>
           <a-button type="primary" @click="closeResultPop('single')" class="bg-697783">닫기</a-button>
         </template>
       </a-modal>
@@ -817,8 +816,6 @@ export default defineComponent({
         this.singleDetail = [];
         this.checkedList = [];
       }
-
-      this.getList();
     },
 
     testmarket() {
