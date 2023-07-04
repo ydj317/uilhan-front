40caa27e4b98f302239af7b0afdb0cdf84d82df8
baichangjie <changjie201@hotmail.com>
2023-07-04 21:55:39 +0800

    임시저장

--- src/components/Detail/description.vue
+++ src/components/Detail/description.vue
@@ -3,19 +3,28 @@
     <!--title-->
     <h1><strong>상세설명</strong></h1>
 
-    <!--button-->
-    <a-button
-      hidden
-      class="button bg-3051d3 originalDetailTrans"
-      type="primary"
-      @click="translatePopup"
-      >상세 이미지번역</a-button
-    >
-
     <!-- 상세설명 편집기 -->
-    <div class="center">
+    <div class="editorDiv">
+      <div class="editorToolbar">
+        <!--button-->
+        <a-button
+            class="button originalDetailTrans"
+            type="primary"
+            @click="translatePopup"
+        >상세 이미지번역</a-button>
+        <a-dropdown>
+          <template #overlay>
+            <a-menu>
+              <a-menu-item @click="addSkuInfo">품목정보 추가</a-menu-item>
+              <a-menu-item @click="">품목정보 제거</a-menu-item>
+            </a-menu>
+          </template>
+          <a-button class="spec-right-button">
+            품목정보 액션
+          </a-button>
+        </a-dropdown>
+      </div>
       <TEditor
-        class="w90"
         ref="editor"
         v-model:value="product.item_detail"
         :productId="Number(product['item_id'])"
@@ -26,8 +35,10 @@
 </template>
 
 <script>
+import { forEach } from "lodash";
 import { mapState } from "vuex";
 import TEditor from "../ImageEditor/TEdtor";
+import { watchEffect } from "vue";
 
 export default {
   name: "productDetailDescription",
@@ -45,8 +56,56 @@ export default {
       aBakDetailImages: {},
     };
   },
-
+  mounted() {
+    this.fetchData();
+  },
   methods: {
+    fetchData() {
+      //품목 이미지, 품목명 변경에 따라 액션
+      watchEffect(() => {
+        this.product.sku.map(item => item.img);
+      });
+    },
+    getOptionTable() {
+      let skuHtml = '<div><table>'
+      let i = 1;
+      let trStartTag = null;
+      let skuLength = this.product.sku.length;
+      forEach(this.product.sku, (item) => {
+        if (i === 1 || i === trStartTag) {
+          trStartTag = i + 4;
+          skuHtml += '<tr>';
+        }
+        skuHtml += `<td><img style="height:100px;width:100px;" src="${item.img}"></td>`;
+        skuHtml += `<td>${item.spec}</td>`;
+        //td 추가
+        if (i === skuLength) {
+          if (skuLength > 4) {
+            for (let j = 0; j < (4 - skuLength % 4); j++) {
+              skuHtml += `<td><img style="height:100px;width:100px;" src=""></td>`;
+              skuHtml += `<td></td>`;
+            }
+          }
+        }
+
+        if (i % 4 === 0) {
+          skuHtml += '</tr>';
+        }
+        i++;
+      });
+      skuHtml += '</table></div>'
+
+      return skuHtml;
+    },
+    addSkuInfo() {
+      if (this.product.sku.length === 0) {
+        return;
+      }
+
+      let skuHtml = this.getOptionTable();
+
+      this.product.item_detail = skuHtml + this.product.item_detail;
+    },
     contentUpdate(tinymce) {
       this.product.item_detail = tinymce.editors[0].getContent();
     },
@@ -94,6 +153,10 @@ export default {
 
     translatePopup() {
       let aImagesUrl = this.getDetailContentsImage();
+      //이미지 없을 경우
+      if (aImagesUrl === false) {
+        return false;
+      }
 
       this.product.bProductDetailsEditor = true;
       this.product.bImageEditorModule = true;
@@ -116,47 +179,21 @@ export default {
     },
   },
 };
-
-class DescriptionClass {
-  constructor() {
-    this.DetailTrans();
-  }
-
-  DetailTrans() {
-    let itimer = setInterval(() => {
-      try {
-        let oImageEditor = document.querySelector('[title="이미지 업로드"]');
-        let tinymce = document.querySelector(".tox-edit-area__iframe");
-        // .contentDocument.querySelector("#tinymce > p");
-
-        if (oImageEditor && tinymce) {
-          clearInterval(itimer);
-
-          let sStyle = `style="width: 22px; height: 22px;"`;
-          let sHtml = `
-            <button aria-label="이미지 업로드" title="이미지 업로드" type="button" tabindex="-1" class="tox-tbtn copyDetailTrans" aria-disabled="false">
-                <span class="tox-icon tox-tbtn__icon-wrap">
-                    <svg ${sStyle} focusable="false" class="" data-icon="form" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M904 512h-56c-4.4 0-8 3.6-8 8v320H184V184h320c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V520c0-4.4-3.6-8-8-8z"></path><path d="M355.9 534.9L354 653.8c-.1 8.9 7.1 16.2 16 16.2h.4l118-2.9c2-.1 4-.9 5.4-2.3l415.9-415c3.1-3.1 3.1-8.2 0-11.3L785.4 114.3c-1.6-1.6-3.6-2.3-5.7-2.3s-4.1.8-5.7 2.3l-415.8 415a8.3 8.3 0 00-2.3 5.6zm63.5 23.6L779.7 199l45.2 45.1-360.5 359.7-45.7 1.1.7-46.4z"></path></svg>
-                </span>
-            </button>`;
-          oImageEditor.insertAdjacentHTML("afterend", sHtml);
-
-          document.querySelector(".copyDetailTrans").onclick = () => {
-            document.querySelector(".originalDetailTrans").click();
-          };
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    }, 500);
-  }
-}
-new DescriptionClass();
 </script>
 
 <style scoped>
+.editorDiv {
+  display: flex;
+  flex-direction: column;
+}
+
+.editorDiv .editorToolbar {
+  margin-right: auto;
+}
+
 .button {
   width: 150px;
   margin-bottom: 10px;
+  margin-right: 10px;
 }
 </style>
