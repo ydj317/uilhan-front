65ef5025e3832d4e9700ecd27cad39b4f319ba1b
rhji02 <limho0202@gmail.com>
2023-07-15 10:48:28 +0800

    fix:['UI']

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
     <router-view/>
-    <notice-popup />
 </template>
 
-<script setup>
-import NoticePopup from "@/views/Template/NoticePopup.vue";
-
-</script>
 <style lang="less">
 </style>
