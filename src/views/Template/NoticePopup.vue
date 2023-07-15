97f49b18240f5ac70f2d5fadc7e9b9983d645e28
rhji02 <limho0202@gmail.com>
2023-07-15 10:49:46 +0800

    rollback:['UI']

---
+++ src/views/Template/NoticePopup.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+const open = ref(true);
+
+const showModal = () => {
+  open.value = true;
+};
+
+const handleOk = (e) => {
+  console.log(e);
+  open.value = false;
+};
+</script>
+
+<template>
+
+  <a-modal v-model:visible="open" title="" @ok="handleOk" :mask="false" :footer="null" >
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </a-modal>
+
+</template>
+
+<style scoped>
+
+</style>
