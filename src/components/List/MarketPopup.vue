9770686fa9c28e3eebd21db91f3c08203fef071a
rhji02 <limho0202@gmail.com>
2023-09-11 14:00:52 +0800

    [rhji02]-bug:[router없어짐]

--- src/components/List/MarketPopup.vue
+++ src/components/List/MarketPopup.vue
@@ -12,6 +12,7 @@ const indicator = ref(true);
 const queryString = decodeURIComponent(route.params.query);
 const params = new URLSearchParams(queryString);
 const queryJson = Object.fromEntries(params);
+const message_v = ref('페이지 이동중 입니다. 잠시만 기다려 주세요.');
 //const queryJson = {'ssi_ix': 4600,'pid':'0008875356','site_id':'ncp_1nn2zx_01'}
 const marketUrls = {
   '11st': "https://www.11st.co.kr/products/",
@@ -61,10 +62,7 @@ const syncProduct = (queryJson) => {
 
     let url = '';
     if(prdDetail.site_code === 'storefarm' || prdDetail.site_code === 'tmon') {
-      message.warning('스마트스토어는 잠시 지원하지 않습니다.');
-      setTimeout(() => {
-        window.close()
-      }, 500)
+      message_v.value = '스마트스토어는 잠시 지원하지 않습니다.';
       //url = marketUrls[prdDetail.site_code] + prdDetail.site_id + '/products/' + queryJson.pid
     } else {
       url = marketUrls[prdDetail.site_code] + prdDetail.market_pid
@@ -81,9 +79,8 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div style="display: flex;justify-content: center;align-items: center;width: 100vw;height: 100vh;">
-  <loading v-model:active="indicator" :can-cancel="false" :is-full-page="true"/>
-    <h3>페이지 이동중 입니다. 잠시만 기다려 주세요.</h3>
+  <div style="display: flex;justify-content: center;align-items: center;gap:30px;flex-direction:column;width: 100vw;height: 100vh;padding-top: 100px;">
+    <h3 v-html="message_v">페이지 이동중 입니다. 잠시만 기다려 주세요.</h3>
   </div>
 </template>
 
