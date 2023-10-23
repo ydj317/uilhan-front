15683979af31b64c26613ca73ef8d3b117242970
rhji02 <limho0202@gmail.com>
2023-10-23 13:05:15 +0800

    update

--- src/views/Order/claim/List.vue
+++ src/views/Order/claim/List.vue
@@ -100,7 +100,14 @@
             <a-table-column title="상품명" dataIndex="prd_name" key="prd_name" />
             <a-table-column title="옵션명" dataIndex="prd_option_name" key="prd_option_name" :width="240" />
             <a-table-column title="수량" dataIndex="quantity" key="quantity" :width="50" />
-            <a-table-column title="클레임사유" dataIndex="claim_code" key="claim_code" :width="120" />
+            <a-table-column title="클레임사유" dataIndex="claim_code" key="claim_code" :width="120">
+              <template #default="{ record }">
+                <a-space>
+                  {{ record.claim_message || '-' }}
+                  {{ record.claim_detail }}
+                </a-space>
+              </template>
+            </a-table-column>
             <a-table-column title="" dataIndex="command" key="command" :width="180">
               <template #default="{ record }">
                 <a-space>
