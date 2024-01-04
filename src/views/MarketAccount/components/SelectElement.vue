517bd7eefd7302b40bc174b6620a99dabc5e46c9
jglee <jeengn1989@outlook.com>
2024-01-04 23:58:20 +0800

    계정 필드 추가함

---
+++ src/views/MarketAccount/components/SelectElement.vue
@@ -0,0 +1,26 @@
+<template>
+	<a-select v-model:value="selectedValue" :style="style">
+		<a-select-option value=""> - 선택해주세요 - </a-select-option>
+		<a-select-option :value="item" v-for="(item, key) in options"
+						 :key="key">{{ item }}</a-select-option>
+	</a-select>
+</template>
+
+<script setup>
+import { computed, defineProps, defineEmits } from 'vue';
+
+// 定义 props 和 emits
+const props = defineProps({
+	options: Object,
+	value: String,
+	style: String,
+});
+
+const emit = defineEmits(['update:value']);
+
+// 计算属性来处理 v-model
+const selectedValue = computed({
+	get: () => props.value,
+	set: (val) => emit('update:value', val)
+});
+</script>
--- src/views/MarketAccount/markets/Sk11st.vue
+++ src/views/MarketAccount/markets/Sk11st.vue
@@ -77,6 +77,44 @@
 						 style="width:160px"/>
 			</a-form-item>
 
+			<a-form-item name="place_purchase" label="구매처"
+						 :rules="[{ required: true, message: '구매처를 선택해 주세요.' }]">
+				<SelectElement :options="placePurchaseList"
+							   v-model:value="state.formData.place_purchase"
+							   :style="'width:350px;'"
+				/>
+			</a-form-item>
+
+			<a-form-item label="발송예정일 템플릿 등록">
+				<a-form-item name="send_method" label="발송유형"
+							 :rules="[{ required: true, message: '발송유형을 선택해 주세요.' }]" :label-col="{ span: 5 }"
+							 :wrapper-col="{ span: 19 }">
+					<SelectElement :options="sendMethodList"
+								   v-model:value="state.formData.send_method"
+								   :style="'width:250px;'"
+					/>
+				</a-form-item>
+
+				<a-form-item name="send_normal" label="평일"
+							 :rules="[{ required: true, message: '평일을 선택해 주세요.' }]" :label-col="{ span: 5 }"
+							 :wrapper-col="{ span: 19 }">
+					오늘 주문완료 건&nbsp;
+					<SelectElement :options="sendDayList"
+								   v-model:value="state.formData.send_normal"
+								   :style="'width:150px;'"
+					/>
+					&nbsp;일 내 발송처리
+				</a-form-item>
+				<a-form-item name="send_saturday" label="토요일" :label-col="{ span: 5 }" :wrapper-col="{ span: 19 }">
+					<a-checkbox-group v-model:value="state.formData.send_saturday">
+						<a-checkbox value="Y" name="send_saturday">발송함</a-checkbox>
+					</a-checkbox-group>
+				</a-form-item>
+				<a-checkbox-group v-model:value="state.formData.send_default_time" style="margin-top: 10px;">
+					<a-checkbox value="Y" name="send_default_time">대표 마감시간으로 설정</a-checkbox>
+				</a-checkbox-group>
+			</a-form-item>
+
 		</div>
 		<a-button class="mt15" @click="goBack">돌아가기</a-button>
 		<a-button class="mt15 ml15" @click="handleSubmit" type="primary">저장</a-button>
@@ -91,6 +129,7 @@ import {useMarketAccountApi} from '@/api/marketAccount';
 import {useAccountJsonApi} from '@/api/accountJson';
 import {CheckCircleOutlined} from '@ant-design/icons-vue';
 import {useRouter} from 'vue-router';
+import SelectElement from '@/views/MarketAccount/components/SelectElement.vue'
 
 const props = defineProps({
 	market_code: {
@@ -102,6 +141,54 @@ const props = defineProps({
 		default: {}
 	}
 });
+
+const placePurchaseList = {
+	A : 'A : 해당 브랜드 정식 도매',
+	B : 'B : 해당 브랜드 직영 온,오프라인 매장(백화점포함)',
+	C : 'C : 오프라인 아울렛',
+	D : 'D : 현지 온라인 쇼핑몰',
+	E : 'E : A~D에 해당되지 않는 구입처(경매 등)',
+}
+
+// TODO 해외직구일 경우 03만 선택 가능함
+const sendMethodList = {
+	// '01' : '일반발송',
+	// '02' : '오늘발송',
+	'03' : '재고확인 후 순차발송(소량재고/주문제작)',
+}
+
+// TODO 해외직구일 경우 필요 없어보임
+const sendTimeList = {
+	"0900": "오전 9시",
+	"1000": "오전 10시",
+	"1100": "오전 11시",
+	"1200": "정오 12시",
+	"1300": "오후 1시",
+	"1400": "오후 2시",
+	"1500": "오후 3시",
+	"1600": "오후 4시",
+	"1700": "오후 5시",
+	"1800": "오후 6시",
+	"1900": "오후 7시",
+	"2000": "오후 8시"
+}
+
+const sendDayList = {
+	// '1' : '1일',
+	// '2' : '2일',
+	'3' : '3일',
+	'4' : '4일',
+	'5' : '5일',
+	'6' : '6일',
+	'7' : '7일',
+	'8' : '8일',
+	'9' : '9일',
+	'10' : '10일',
+	'11' : '11일',
+	'12' : '12일',
+	'13' : '13일',
+	'14' : '14일',
+}
 
 const router = useRouter();
 
@@ -126,6 +213,15 @@ const state = reactive({
 		jeju_add_delivery_price_round_trip: null, // 제주 외 도서산간 추가 배송비
 		return_delivery_price: null, // 반품배송비(편도)
 		return_delivery_price_round_trip: null, // 반품배송비(왕복)
+		place_purchase: '',	// 구입처
+
+		// 발송예정일 파람들
+		send_method: '', // 발송방법
+		send_normal: '', // 일반발송
+		send_today_end_time: '', // 오늘발송인경우 당일 주문마감시간 TODO 해외직구일 경우 필요 없어보임
+		send_saturday: 'N', // 토요일 발송여부
+		send_saturday_end_time: '', // 토요일 주문마감시간 TODO 해외직구일 경우 필요 없어보임
+		send_default_time: 'N', // 대표 마감시간으로 설정
 
 	},
 
@@ -166,6 +262,13 @@ const initFormData = () => {
 		state.formData.jeju_add_delivery_price_round_trip = accountInfo['marketData']?.jeju_add_delivery_price_round_trip;
 		state.formData.return_delivery_price = accountInfo['marketData']?.return_delivery_price;
 		state.formData.return_delivery_price_round_trip = accountInfo['marketData']?.return_delivery_price_round_trip;
+
+		state.formData.place_purchase = accountInfo['marketData']?.place_purchase;
+		state.formData.send_method = accountInfo['marketData']?.send_method;
+		state.formData.send_normal = accountInfo['marketData']?.send_normal;
+		state.formData.send_saturday = accountInfo['marketData']?.send_saturday;
+		state.formData.send_default_time = accountInfo['marketData']?.send_default_time;
+
 	}
 }
 
