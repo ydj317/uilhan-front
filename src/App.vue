ad6cd058da3e34ed19c044286f21c3c6fa8d8d59
scson <763761452@qq.com>
2024-01-12 09:34:17 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
+  <a-config-provider :getPopupContainer="getPopupContainer">
     <router-view/>
+  </a-config-provider>
 </template>
 
-<script setup>
-
+<script>
+  export default {
+    methods: {
+      getPopupContainer(el, dialogContext) {
+        console.log('==0==')
+        console.log(el)
+        console.log(dialogContext)
+        return document.body;
+      },
+    },
+  };
 </script>
 
 <style scoped>
