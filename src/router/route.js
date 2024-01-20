3065c8c4c24e89e2b9b9c7868c1880e98b2fc4fb
scson <763761452@qq.com>
2024-01-20 16:54:03 +0800

    대시보드 작업중 20%

--- src/router/route.js
+++ src/router/route.js
@@ -36,6 +36,17 @@ export const menus = [{
             }
         },
         {
+            path: "/dashboard_bak",
+            name: "dashboard_bak",
+            component: () => import("@/views/Dashboard_bak.vue"),
+            meta: {
+                title: "대시보드_bak",
+                isHide: false,
+                roles: ["ROLE_USER"],
+                icon: DashboardOutlined
+            }
+        },
+        {
             path: "/product",
             name: "product",
             component: () => import("@/views/Product/List"),
