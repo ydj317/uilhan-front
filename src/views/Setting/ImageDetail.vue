3690c64c6e326d63e71477ad56eae4dae4c65973
jhwang <609267130@qq.com>
2024-03-07 14:11:56 +0800

    1.switch开关关闭状态时，禁止编辑 2.增加저장按钮,点击后保存修改

--- src/views/Setting/ImageDetail.vue
+++ src/views/Setting/ImageDetail.vue
@@ -9,76 +9,49 @@
           </a-space>
         </template>
 
-        <a-switch v-model:checked="formState.settingDatas.imgSecection.image_upload_switch" @change="handleSaveUserData"
+        <a-switch v-model:checked="formState.settingDatas.description_option.top_bottom_image.use"
                   checked-children="on" un-checked-children="off" />
 
-        <div v-if="formState.settingDatas.imgSecection.image_upload_switch">
+        <div class="upload-container" :class="{ 'is-disabled': !formState.settingDatas.description_option.top_bottom_image.use }">
             <span class="image-fix">
               <span class="title">상단 고정 이미지</span>
               <a-upload
-                  v-model:file-list="fileListTop"
                   name="avatar"
+                  :headers="HEADER"
+                  :multiple="true"
+                  :max-count="1"
                   list-type="picture-card"
-                  :headers="formState.uploadHeaders"
-                  :max-count="3"
-                  :action="formState.uploadUrl"
-                  @change="handleChangeTop"
+                  :beforeUpload="validateUploadImage"
+                  :customRequest="(options) => uploadImage(options, 'top')"
+                  v-model:file-list="fileListTop"
               >
-                 <template v-if="fileListTop.length < 3">
                   <div>
                     <loading-outlined v-if="loadingTop" />
                     <plus-outlined v-else />
                     <div class="ant-upload-text">Upload</div>
                   </div>
-                </template>
               </a-upload>
             </span>
             <span class="image-fix">
               <span class="title">하단 고정 이미지</span>
               <a-upload
-                  v-model:file-list="fileListBottom"
                   name="avatar"
+                  :headers="HEADER"
+                  :multiple="true"
+                  :max-count="1"
                   list-type="picture-card"
-                  :headers="formState.uploadHeaders"
-                  :max-count="3"
-                  :action="formState.uploadUrl"
-                  @change="handleChangeBottom"
+                  :beforeUpload="validateUploadImage"
+                  :customRequest="(options) => uploadImage(options, 'bottom')"
+                  v-model:file-list="fileListBottom"
               >
-                 <template v-if="fileListBottom.length < 3">
-                  <div>
-                    <loading-outlined v-if="loadingBottom" />
-                    <plus-outlined v-else />
-                    <div class="ant-upload-text">Upload</div>
-                  </div>
-                </template>
+
+                <div>
+                  <loading-outlined v-if="loadingBottom" />
+                  <plus-outlined v-else />
+                  <div class="ant-upload-text">Upload</div>
+                </div>
               </a-upload>
             </span>
-        </div>
-      </a-descriptions-item>
-
-      <a-descriptions-item>
-        <template #label>
-          <a-space>
-            <span>옵션 테이블 설정</span>
-          </a-space>
-        </template>
-
-        <a-switch v-model:checked="formState.settingDatas.option.option_table_switch" @change="handleSaveUserData" checked-children="on" un-checked-children="off" />
-        <div class="select" v-if="formState.settingDatas.option.option_table_switch">
-          <!-- 第一个下拉菜单 -->
-          <a-select v-model:value="formState.settingDatas.option.option_select_value1" style="width: 120px">
-            <a-select-option v-for="selectOption in formState.settingDatas.option.optionTableSelectOption1" :key="selectOption.value"
-                             :value="selectOption.value">
-              {{ selectOption.label }}
-            </a-select-option>
-          </a-select>
-          <!-- 第二个下拉菜单 -->
-          <a-select v-model:value="formState.settingDatas.option.option_select_value2" style="width: 120px">
-            <a-select-option v-for="selectOption in formState.settingDatas.option.optionTableSelectOption2" :key="selectOption.value"
-                             :value="selectOption.value">
-              {{ selectOption.label }}
-            </a-select-option>
-          </a-select>
         </div>
       </a-descriptions-item>
 
@@ -88,124 +61,205 @@
             <span>동영상 노출 설정</span>
           </a-space>
         </template>
-        <a-switch v-model:checked="formState.settingDatas.show_video" @change="handleSaveUserData" checked-children="on" un-checked-children="off" />
+        <a-switch v-model:checked="formState.settingDatas.description_option.show_video"
+                  checked-children="on" un-checked-children="off" />
+      </a-descriptions-item>
+
+      <a-descriptions-item>
+        <template #label>
+          <a-space>
+            <span>옵션 테이블 설정</span>
+          </a-space>
+        </template>
+
+        <a-switch v-model:checked="formState.settingDatas.description_option.option_table.use"
+                   checked-children="on" un-checked-children="off" />
+        <div class="select upload-container" :class="{ 'is-disabled': !formState.settingDatas.description_option.option_table.use }">
+          <!-- 第一个下拉菜单 -->
+          <a-select v-model:value="formState.settingDatas.description_option.option_table.show_position"
+                    style="width: 120px">
+            <a-select-option v-for="selectOption in showPosition" :key="selectOption.value"
+                             :value="selectOption.value">
+              {{ selectOption.label }}
+            </a-select-option>
+          </a-select>
+          <!-- 第二个下拉菜单 -->
+          <a-select v-model:value="formState.settingDatas.description_option.option_table.column_length"
+                    style="width: 120px">
+            <a-select-option v-for="selectOption in columnLength" :key="selectOption.value"
+                             :value="selectOption.value">
+              {{ selectOption.label }}
+            </a-select-option>
+          </a-select>
+        </div>
       </a-descriptions-item>
     </a-descriptions>
+    <div style="display: flex;justify-content: center;margin-top: 20px;">
+      <a-button type="primary" html-type="submit" @click="saveUserInfo">저장</a-button>
+    </div>
   </a-card>
 </template>
 
 <script setup>
 import {AuthRequest} from "@/util/request";
 
-import {onMounted, reactive, ref, watch} from "vue";
+import {onMounted, reactive, ref } from "vue";
 import Cookie from "js-cookie";
 import {message} from "ant-design-vue";
 import {useUserApi} from "@/api/user";
 import {PlusOutlined, LoadingOutlined} from "@ant-design/icons-vue";
+import router from "@/router";
+
+const showPosition = reactive([
+  { label: '상단에', value: 'top' },
+  { label: '하단에', value: 'bottom' },
+]);
+
+const columnLength = reactive([
+  { label: '두줄로 추가', value: 2 },
+  { label: '네줄로 추가', value: 4 },
+]);
 
 
 //switch
 const formState = reactive({
   settingDatas: {
-    imgSecection:{
-      image_upload_switch: false,
-      image_upload_image_top: 'top',
-      image_upload_image_bottom: 'bottom'
-    },
-    option:{
-      option_table_switch:false,
-      option_select_value1: 'top',  // 直接在对象内设置默认值
-      option_select_value2: 'table_two_column',
+    description_option:{
+      top_bottom_image : {
+        use : false,
+        top_image_url: "",
+        bottom_image_url: "",
+      },
 
-      optionTableSelectOption1: [  // 直接定义为数组，无需ref
-        { label: '상단에', value: 'top' },
-        { label: '하단에', value: 'bottom' },
-      ],
-      optionTableSelectOption2: [  // 直接定义为数组，无需ref
-        { label: '두줄로 추가', value: 'table_two_column' },
-        { label: '네줄로 추가', value: 'table_four_column' },
-      ]
+      show_video: false,
+
+      option_table: {
+        use: true,
+        show_position : "top",
+        column_length :2
+      },
     },
-    show_video: false
   },
-
-
-  //upload
-  uploadUrl: process.env.VUE_APP_API_URL + '/api/user/uploadImage',
   loading: false,
-  uploadHeaders: {
-    token: Cookie.get("token")
-  },
 });
+
+// const topImageUrl = "https:\\/\\/worldlink.mev\\/uploads\\/25778155-65e915fd0fa48.jpg";
+// const fileListTop = ref([
+//   {
+//     uid: '-1', // 通常使用文件的唯一标识符，这里只是示例
+//     name: 'Top Image.jpg',
+//     status: 'done', // 表示文件已经上传
+//     url: topImageUrl, // 从后端获取的图片URL
+//   }
+// ]);
 
 const fileListTop = ref([]);
 const fileListBottom = ref([]);
 const loadingTop = ref(false);
 const loadingBottom = ref(false);
-const imageUrl = ref('');
+// const imageUrl = ref('');
+// const loading = ref(false);
 
-//option
-// const selectOptionValue1 = ref('top'); //默认
-// const optionTableSelectOption1 = ref([
-//   { label: '상단에', value: 'top' },
-//   { label: '하단에', value: 'bottom' },
-// ]);
-//
-// // const selectOptionValue2 = ref('table_two_column');
-// const optionTableSelectOption2 = ref([
-//   { label: '두줄로 추가', value: 'table_two_column' },
-//   { label: '네줄로 추가', value: 'table_four_column' },
-// ]);
+const HEADER =ref({
+    token: Cookie.get("token"),
+    "Content-Type": "multipart/form-data",
+})
 
-// watch(() => formState.settingDatas.option.option_select_value1, (newValue) => {
-//   console.log('New value for option_select_value1:', newValue);
-// });
-//
-// watch(() => formState.settingDatas.option.option_select_value2, (newValue) => {
-//   console.log('New value for option_select_value2:', newValue);
-// });
+function uploadImage(option,position) {
+  const formData = new FormData();
+  formData.append("file", option.file);
+  formData.append("image_type", "product");
+  formData.append("relation_type", "product");
 
-const handleChangeTop = ({ file, fileList: newFileList }) => {
-  if (file.status === 'uploading') {
-    loadingTop.value = true;
-  } else {
-    loadingTop.value = false;
-    fileListTop.value = newFileList;
-  }
-};
+  if (position === 'top') loadingTop.value = true;
+  else loadingBottom.value = true;
 
-const handleChangeBottom = ({ file, fileList: newFileList }) => {
-  if (file.status === 'uploading') {
-    loadingBottom.value = true;
-  } else {
-    loadingBottom.value = false;
-    fileListBottom.value = newFileList;
-  }
-};
+  AuthRequest.post(
+      process.env.VUE_APP_API_URL + "/api/image",
+      formData
+  ).then((res) => {
+    if (res.status !== "2000") {
+      message.error(res.message);
+      return false;
+    }
 
-const handleSaveUserData = () => {
-  useUserApi().saveUserData(formState.settingDatas).then((res) => {
-    console.log('Data saved successfully');
+    let response = res.data;
+    if (!response || Object.keys(response).length === 0) {
+      message.error("upload failed");
+      return false;
+    }
+
+    message.success(res.message);
+
+    // 上传成功处理逻辑
+    let imgUrl = res.data.img_url;
+    let newFileList = [{
+      uid: '-1', // 唯一标识，可以是上传文件的ID
+      name: 'image.png', // 文件名
+      status: 'done', // 状态有：uploading, done, error, removed
+      url: imgUrl, // 文件访问路径
+    }];
+
+    // 根据位置更新状态和文件列表
+    if (position === 'top') {
+      formState.settingDatas.description_option.top_bottom_image.top_image_url = imgUrl;
+      fileListTop.value = newFileList; // 更新文件列表
+      loadingTop.value = false; // 停止加载状态
+    } else if (position === 'bottom') {
+      formState.settingDatas.description_option.top_bottom_image.bottom_image_url = imgUrl;
+      fileListBottom.value = newFileList;
+      loadingBottom.value = false;
+    }
+
+    // // 保存到数据库
+    // handleSaveUserData();
+
   }).catch((error) => {
-      console.error('Failed to save data:', error);
+    message.error(`Upload failed: ${error.message}`);
+    if (position === 'top') loadingTop.value = false;
+    else loadingBottom.value = false;
   });
 }
 
-// function fetchUserUploadImage() {
-//   AuthRequest.get(process.env.VUE_APP_API_URL + "/api/user/getUploadImage")
-//       .then(response => {
-//         console.log(response);
-//         const  avatarFilename  = response.avatar_filename;
-//         console.log(avatarFilename);
-//         if (avatarFilename) {
-//           imageUrl.value = `/uploads/${avatarFilename}`;
-//           console.log(imageUrl.value);
-//         }
-//       })
-//       .catch(error => {
-//         console.error('Failed to fetch user profile:', error);
-//       });
+function validateUploadImage(file) {
+  const isJPG = file.type === "image/jpeg";
+  const isJPEG = file.type === "image/jpeg";
+  const isGIF = file.type === "image/gif";
+  const isPNG = file.type === "image/png";
+
+  if (!(isJPG || isJPEG || isPNG || isGIF)) {
+    message.warning("허용되는 이미지 격식이 아닙니다.");
+    return false;
+  }
+
+  return true;
+}
+
+// const handleSaveUserData = () => {
+//   useUserApi().saveUserData(formState.settingDatas).then((res) => {
+//     console.log('Data saved successfully');
+//   }).catch((error) => {
+//       console.error('Failed to save data:', error);
+//   });
 // }
+
+function saveUserInfo(){
+  AuthRequest.post(process.env.VUE_APP_API_URL + "/api/updateUserDetail").then((res) => {
+    if (res.status !== '2000') {
+      message.error(res.message)
+      return false;
+    }
+
+    message.success(res.message);
+
+    useUserApi().saveUserData(formState.settingDatas).then((res) => {
+      console.log('Data saved successfully');
+    }).catch((error) => {
+      console.error('Failed to save data:', error);
+    });
+  });
+}
+
 
 function getUserInfoData() {
   formState.loading = true;
@@ -223,14 +277,17 @@ function getUserInfoData() {
       return false;
     }
 
-    if (user_data.imgSecection=== undefined) {
-      formState.loading = false;
-      return false;
+    // 直接赋值可能不会触发更新，特别是对于深层次的对象
+    if (user_data.description_option && user_data.description_option.top_bottom_image) {
+      formState.settingDatas.description_option.top_bottom_image = { ...user_data.description_option.top_bottom_image };
     }
 
-    if (user_data.option === undefined) {
-      formState.loading = false;
-      return false;
+    if (user_data.description_option && user_data.description_option.show_video) {
+      formState.settingDatas.description_option.show_video = { ...user_data.description_option.show_video };
+    }
+
+    if (user_data.description_option && user_data.description_option.option_table) {
+      formState.settingDatas.description_option.option_table = { ...user_data.description_option.option_table };
     }
 
     formState.settingDatas = user_data;
@@ -240,9 +297,7 @@ function getUserInfoData() {
 
   });
 }
-
 onMounted(() => {
-  // fetchUserUploadImage();
   getUserInfoData();
 });
 
@@ -273,6 +328,11 @@ onMounted(() => {
   }
   .ant-upload-select-picture-card{
     margin-top:10px !important;
+  }
+
+  .upload-container.is-disabled {
+    pointer-events: none;
+    opacity: 0.5;
   }
 
 </style>
