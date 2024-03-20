c3c4da3c4d7364457bf7fd58a57bec53073a64c4
yl01 <986847608@qq.com>
2024-03-20 09:50:24 +0800

    yl

--- src/views/Product/DetailPopup.vue
+++ src/views/Product/DetailPopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-modal v-model:open="localvisible" title="상품상세" width="1200px" centered :maskClosable="false">
+  <a-modal v-model:open="localvisible" title="상품상세" width="100%" centered :maskClosable="false" wrap-class-name="full-modal">
     <a-tabs v-model:activeKey="activeKey"
             :tabBarGutter="0.5"
             type="card"
