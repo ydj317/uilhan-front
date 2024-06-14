5b899d613d185cc9093016ef10774d27929d3b77
gmkim <371169323@qq.com>
2024-06-14 15:21:02 +0800

    해주몰만 서비스 결제 풀어줌

--- src/router/route.js
+++ src/router/route.js
@@ -431,8 +431,9 @@ export const menus = [{
             component: () => import("@/views/User/ServicePayAi.vue"),
             meta: {
                 title: "️AI 서비스 결제",
-                isHide: true,
+                isHide: false,
                 roles: ["ROLE_ADMIN", "ROLE_USER"],
+                ids: ["haeju"],
                 icon: RobotOutlined
             },
         },
