b56cec3b7bb330e7b47475af2dfd86ebfbbe5b9b
gmkim <371169323@qq.com>
2024-06-17 15:05:23 +0800

    연동할때 로딩을 전페이지 말고 연동페이지에만 띄우고 팝업창 닫을수 있게 수정

--- src/views/Product/List/ModalSingleSync/ModalSingleSync.vue
+++ src/views/Product/List/ModalSingleSync/ModalSingleSync.vue
@@ -1,6 +1,6 @@
 <template>
-  <FullPageLoading v-model:show="loading" />
   <a-modal width="1000px" :open="show" @update:open="$emit('update:show', false)" centered>
+    <loading style="z-index: 999" v-model:active="syncLoading" :can-cancel="false" :is-full-page="false" />
     <template #title>
       선택상품 등록
       <a-tooltip>
@@ -74,8 +74,8 @@ import {ref, toRefs, watch} from "vue";
 import {getLogoSrc} from "@/util/functions";
 import {AuthRequest} from "@/util/request";
 import {message} from "ant-design-vue";
-import FullPageLoading from "@/components/FullPageLoading.vue";
 import {ServiceProduct} from "@/services/product/ServiceProduct";
+import Loading from "vue-loading-overlay";
 
 const SYNC_COLUMNS_CONFIG = [
   {
@@ -106,7 +106,7 @@ watch(show, val => {
   }
 })
 
-const loading = ref(false)
+const syncLoading = ref(false)
 const syncSelectedRowKeys = ref([])
 
 function onSyncSelectChange(selectedRowKeys) {
@@ -114,7 +114,7 @@ function onSyncSelectChange(selectedRowKeys) {
 }
 
 async function approvalCheck(market_id) {
-  loading.value = true;
+  syncLoading.value = true;
 
   try {
     let res = await AuthRequest.post(process.env.VUE_APP_API_URL + "/api/approval_check", {
@@ -123,19 +123,19 @@ async function approvalCheck(market_id) {
 
     if (res.status !== "2000") {
       message.error(res.message);
-      loading.value = false;
+      syncLoading.value = false;
       return false;
     }
 
     if (res.data !== undefined && res.data.length === 0) {
       message.error("해당요청에 오류가 발생하였습니다. \n재시도하여 오류가 지속될시 관리자에게 문의하여 주십시오.");
-      loading.value = false;
+      syncLoading.value = false;
       return false;
     }
 
     if (res.data.message !== undefined && res.data.message !== '') {
       message.warning(res.data.message);
-      loading.value = false;
+      syncLoading.value = false;
       return false;
     }
 
@@ -159,12 +159,12 @@ async function approvalCheck(market_id) {
       }
     }
 
-    loading.value = false;
+    syncLoading.value = false;
 
     return true;
   } catch (e) {
     message.error(e.message);
-    loading.value = false;
+    syncLoading.value = false;
     return false;
   }
 }
@@ -182,13 +182,13 @@ async function sendMarket() {
     return
   }
 
-  loading.value = true
+  syncLoading.value = true
   const result = await ServiceProduct.sendMarket(productList, accountList, props.smartStoreCategory)
   if (result !== false) {
     emit('result', result)
     closeModal()
   }
-  loading.value = false
+  syncLoading.value = false
 }
 
 async function loadSyncInfo() {
