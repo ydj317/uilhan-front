e45e3630ca80c5bbe9de7b04cd7037bfb3851dba
Hinatailiang <986847608@qq.com>
2024-07-16 13:13:32 +0800

    修复删除图片后再翻译显示已删除图片问题

--- src/components/Detail/ImageTranslateTools.vue
+++ src/components/Detail/ImageTranslateTools.vue
@@ -71,13 +71,16 @@ export default defineComponent({
   methods: {
     //图片处理回调
     async handleTranslateCallback(oTranslateInfo) {
-      const {action,requestId,allSort,base64} = oTranslateInfo;
+      const {action,requestId,requestIds,allSort,base64} = oTranslateInfo;
       // console.log('oTranslateInfo',oTranslateInfo);
       switch (action){
         case 'upload':
           this.uploadImage(base64,()=>{
             this.translateImage({type: 2,action});
           });
+          break;
+        case 'delete':
+          this.translateImage({type: 4,requestIds,action});
           break;
         case 'translate':
           await this.translateImage({type: 3,isTranslate: true,requestId,action});
@@ -99,7 +102,7 @@ export default defineComponent({
     },
     async translateImage(option,back) {
       back = back || function(){}
-      const {type,isTranslate = false,requestId,action='',imglist} = option;
+      const {type,isTranslate = false,requestId,requestIds,action='',imglist} = option;
       // console.log('translateImage-option',option);
       if(imglist){
         this.localTranslateImageList = imglist;
@@ -118,6 +121,12 @@ export default defineComponent({
           break;
         case 3://翻译图片
           images = this.localTranslateImageList.filter(item =>item.request_id === requestId);
+          break;
+        case 4://删除图片
+          let localTranslateImageList = requestIds.map(request_id=>{
+            return this.localTranslateImageList.find(item=>item.request_id==request_id);
+          })
+          this.localTranslateImageList = localTranslateImageList;
           break;
         default:
           images = [];
