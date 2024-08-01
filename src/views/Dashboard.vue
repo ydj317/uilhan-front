f3feb74caacf6d38577f662cf82ba518d3be2339
Hinatailiang <986847608@qq.com>
2024-08-01 15:46:54 +0800

    样式问题

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
-  <a-flex class="dashboard">
-    <a-flex vertical class="dashboard-left mb40">
+  <a-flex class="dashboard fl-lb">
+    <a-flex vertical class="dashboard-left">
       <div class="slide-wrap">
         <a-carousel autoplay ref="carousel">
                     <div v-for="image in formState.bannerList" class="fl-tc carousel-slide"><img :src="image"/></div>
@@ -77,7 +77,7 @@
         </a-skeleton>
       </a-flex>
     </a-flex>
-    <a-flex vertical class="dashboard-right ml40">
+    <a-flex vertical class="dashboard-right">
       <div class="notice-wrap fs14">
         <a-tabs v-model:activeKey="formState.noticeActiveTab">
           <a-tab-pane key="1" tab="유일 공지">
@@ -689,8 +689,11 @@ onBeforeUnmount(() => {
 :deep(.upload-wrap .ant-collapse-content-box) {
   padding-block: 0!important;
 }
+.dashboard{
+  margin: 0 20px 40px;
+}
  .dashboard-left{
-  width: calc(100% - 400px);
+  width: calc(100% - 440px);
 }
  .slide-wrap{
    position: relative;
@@ -801,7 +804,7 @@ onBeforeUnmount(() => {
 .dashboard-right{
   width: 400px;
   display: flex;
-  justify-content: inherit!important;
+  justify-content: flex-start;
 }
 .notice-wrap{
   width: 100%;
