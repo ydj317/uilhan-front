5d98299c483a20e62316ce98d86b32faccd5aa80
Hinatailiang <986847608@qq.com>
2024-08-30 14:21:31 +0800

    消息弹层更新

--- src/views/Template/NoticePopup.vue
+++ src/views/Template/NoticePopup.vue
@@ -1,135 +1,90 @@
+<template>
+  <template v-for="(item,index) in state.list" :key="index">
+    <a-modal :key="index" :title="item.title" :mask="false" v-model:open="item['open' + item.id]" @cancel="modalCancel" v-if="state.currentRoute === item.menu && !item.show && todayShow(item.id)" style="margin-top:-50px;width: 730px">
+      <div style="text-align: center;height: 750px;overflow: scroll;overflow-x: unset;" v-html="item.content">
+      </div>
+      <template #footer>
+        <a-checkbox @change="closeNotice(item.id)" style="color: #999999">오늘 하루 보이지 않기</a-checkbox>
+      </template>
+    </a-modal>
+  </template>
+</template>
 <script setup>
-import { onBeforeMount,onMounted, reactive, ref, watch} from "vue";
+import { reactive, watch, onMounted, onUnmounted } from "vue";
 import { AuthRequest } from "@/util/request";
 import { message } from "ant-design-vue";
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
-const open = ref(true);
-let indicator = ref(false);
-
-const dataSource = reactive({
-  data: [],
-  open:[],
-  visibleDays:[],
+const state = reactive({
+  list: [],
   currentRoute: '',
-  popupShownOnDashboard: false
 });
+state.currentRoute = route.path;
 
-dataSource.currentRoute = route.path;
-
-const shouldShowPopup = (id) => {
-  // const lastPopupTimestamp = localStorage.getItem('lastPopupTimestamp' + id);
-  // if (!lastPopupTimestamp) {
-  //   return true
-  // } else {
-  //   const lastTimestamp = parseInt(lastPopupTimestamp, 10);
-  //   const currentTimestamp = Date.now();
-  //   const oneWeekInMilliseconds = 3 * 24 * 60 * 60 * 1000; // 7天的毫秒数
-  //
-  //   return currentTimestamp - lastTimestamp >= oneWeekInMilliseconds;
-  // }
-
-  const lastTimestamp = localStorage.getItem('lastPopupTimestamp' + id);
-  if (!lastTimestamp) return true;
-  const now = Date.now();
-  return now > parseInt(lastTimestamp);
-}
-
-const checkPopupDisplay = () => {
-  if (dataSource.currentRoute === '/dashboard') {
-    if (sessionStorage.getItem('dashboardRefreshed') === 'true') {
-      // 如果当前 sessionStorage 中标记为已刷新，则直接获取弹窗数据
-      getNoticePopupList();
-      sessionStorage.removeItem('dashboardRefreshed');
-    } else {
-      dataSource.popupShownOnDashboard = false;
-    }
-  } else {
-    // 如果当前不在 dashboard 页面，则清除标记
-    dataSource.popupShownOnDashboard = false;
-  }
-};
-
-const handleCancel = (id) => {
-  if(dataSource.visibleDays['visibal'+id] === true){
-    // const currentTimestamp = Date.now();
-    // localStorage.setItem('lastPopupTimestamp' + id, currentTimestamp.toString());
-
-    const now = new Date();
-    const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59).getTime();
-    localStorage.setItem('lastPopupTimestamp' + id, endOfDay.toString());
-  }
-  dataSource.open['open' + id] = false;
-};
-
-const getNoticePopupList = () => {
-
+const getNoticePopupList = async () => {
   AuthRequest.get(process.env.VUE_APP_API_URL + '/api/board/noticePopupList').then((res) => {
     if (res.status !== '2000') {
       message.error(res.message)
-      indicator.value = false;
       return false;
     }
-
-    dataSource.data = res.data
-
-    dataSource.data.forEach((item) => {
-      dataSource.open['open'+item.id] = ref(true);
-      dataSource.visibleDays['visibal'+item.id] = ref(false);
+    state.list = res.data.map((item) => {
+      item.show = false;
+      item['open' + item.id] = true;
+      return item;
     })
+    localStorage.setItem('noticeList', JSON.stringify(state.list));
   }).catch((error) => {
     message.error(error.message);
     return false;
   });
 }
-
-//启动时检查并清理过期的时间戳
-const clearExpiredPopupTimestamps = () => {
-  const now = Date.now();
-  dataSource.data.forEach(item => {
-    const lastTimestamp = localStorage.getItem('lastPopupTimestamp' + item.id);
-    if (lastTimestamp && now > parseInt(lastTimestamp)) {
-      localStorage.removeItem('lastPopupTimestamp' + item.id);
-    }
-  });
+const closeNotice = (id) => {
+  const tomorrowTime = getMidnightTime();
+  localStorage.setItem('noticeClose_'+id, tomorrowTime);
 };
-
-const handleBeforeUnload = () => {
-  if (dataSource.currentRoute === '/dashboard') {
-    sessionStorage.setItem('dashboardRefreshed', 'true');
+const todayShow = (id) => {
+  let todayTime = Date.now();
+  let tomorrowTime = localStorage.getItem('noticeClose_'+id);
+  if(!tomorrowTime){
+    return true;
   }
+  if(todayTime > tomorrowTime){
+    localStorage.removeItem('noticeClose_' + id);
+    return true;
+  }
+  return false;
+};
+const getMidnightTime = () => {
+  const now = new Date();
+  const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0, 0);
+  return tomorrow.getTime();
 };
 
+const modalCancel = () => {
+  state.list = state.list.map(item=>{
+    if(state.currentRoute == item.menu && !item.show){
+      item.show = true;
+    }
+    return {...item};
+  })
+  localStorage.setItem('noticeList', JSON.stringify(state.list));
+};
 watch(route, (newRoute) => {
-  dataSource.currentRoute = newRoute.path;
-  checkPopupDisplay();
+  state.currentRoute = newRoute.path;
+  const noticeList = localStorage.getItem('noticeList');
+  noticeList ? state.list = JSON.parse(noticeList) : getNoticePopupList();
 });
-
-onBeforeMount(() => {
-  // getNoticePopupList()
-  window.removeEventListener('beforeunload', handleBeforeUnload);
-});
-
+const  refresh = async () => {
+  localStorage.removeItem('noticeList');
+};
 onMounted(() => {
-  dataSource.currentRoute = route.path;
-  checkPopupDisplay();
-  clearExpiredPopupTimestamps();
-  window.addEventListener('beforeunload', handleBeforeUnload);
+  window.addEventListener('beforeunload', refresh);
+});
+onUnmounted(() => {
+  window.removeEventListener('beforeunload', refresh);
 });
 </script>
-
-<template>
-  <template v-for="(item,index) in dataSource.data" :key="index">
-    <a-modal :key="index" v-if="shouldShowPopup(item.id) && dataSource.currentRoute === '/dashboard'" v-model:open="dataSource.open[`open${item.id}`]" :title="item.title" @cancel="handleCancel(item.id)" :mask="false" style="margin-top:-50px;width: 730px">
-      <div style="text-align: center;height: 750px;overflow: scroll;overflow-x: unset;" v-html="item.content">
-      </div>
-      <template #footer>
-        <a-checkbox v-model:checked="dataSource.visibleDays['visibal'+item.id]" style="color: #999999">오늘 하루 보이지 않기</a-checkbox>
-      </template>
-    </a-modal>
-  </template>
-</template>
 
 <style scoped>
 
