c7c92154426596ccce9ca3f9b8dfcb9f9d896e25
gmkim <371169323@qq.com>
2024-10-16 10:30:12 +0800

    #528 이벤트플랜 사용중 유저 추가서비스만 별도 구매 불가하도록 수정 https://ones.cn/project/#/team/YEhe758P/task/App17VwPDYBYkBdK

--- src/views/User/ServicesPay.vue
+++ src/views/User/ServicesPay.vue
@@ -215,6 +215,7 @@ const state = reactive({
       title:'이미지 자동 번역',
       checkList:['상세페이지 전체 자동번역','옵션 이미지 10장 자동번역'],
       radioList:[
+        {text:'50개/일',value:'100000', planName: "Extra1PlanAutoImageCount50"},
         {text:'100개/일',value:'100000', planName: "Extra1PlanAutoImageCount100"},
         {text:'200개/일',value:'200000', planName: "Extra1PlanAutoImageCount200"},
         {text:'300개/일',value:'300000', planName: "Extra1PlanAutoImageCount300"},
@@ -323,7 +324,9 @@ onBeforeMount(() => {
         }
 
         switch (ele.name){
-
+          case 'Extra1PlanAutoImageCount50':
+            text = '50개/일';
+            break;
           case 'Extra1PlanAutoImageCount100':
             text = '100개/일';
             break;
