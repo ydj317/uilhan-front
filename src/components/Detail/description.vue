05abade2a9e327ff637a51216a496ee2831910a8
Hinatailiang <986847608@qq.com>
2024-10-24 14:25:50 +0800

    规格名称不换行问题

--- src/components/Detail/description.vue
+++ src/components/Detail/description.vue
@@ -421,7 +421,7 @@ export default {
         html += `
         <span style="display: flex !important;flex-direction: column !important; width: ${imgWrapWidth} !important;padding: ${imgPaddingWidth} !important;">
         <img src="${item.img}" style="width: 100% !important;border: 1px solid #e8e8e8 !important;border-bottom: 0 !important;">
-        <span style="width: 100% !important;display: flex !important;align-items: center !important;justify-content: center !important;padding: 15px 0 !important;font-size: 16px !important;border: 1px solid #e8e8e8 !important;">${item.spec}</span>
+        <span style="width: 100% !important;display: flex !important;align-items: center !important;justify-content: center !important;padding: 15px 0 !important;font-size: 16px !important;border: 1px solid #e8e8e8 !important;word-break: break-all !important;">${item.spec}</span>
         </span>`;
       });
       html += "</span>";
